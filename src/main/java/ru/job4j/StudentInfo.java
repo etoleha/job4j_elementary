6bf5fd0a565618583b6f0a65482fe2ccedf6d33d
Alexey Lamtyugin <leshalamtyugin@gmail.com>
2024-09-30 23:31:17 +0400

    Разбор первой программы по "взрослому" [#505151]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Alexey Lamtyugin");
+        System.out.println("29.08.1993");
+    }
+}
