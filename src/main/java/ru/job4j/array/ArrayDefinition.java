4fa5cfd1c44f520c34dbf79166b959051dfb0109
Alexey Lamtyugin <leshalamtyugin@gmail.com>
2024-10-08 13:57:31 +0400

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,6 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Atos";
+        names[1] = "Portos";
+        names[2] = "Aramis";
+        names[3] = "Denis";
+        for (String name : names) {
+            System.out.println(name);
+        }
         System.out.println("ages length = " + ages.length);
         System.out.println("surnames length = " + surnames.length);
         System.out.println("prices length = " + prices.length);
