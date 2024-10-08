a737933dd6b7119b35b9fb377115b6539fdee80a
Alexey Lamtyugin <leshalamtyugin@gmail.com>
2024-10-08 14:15:47 +0400

    Массивы в тестах [#505226]

---
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FindLoopTest {
+    @Test
+    void whenArrayHas5Then0() {
+        int[] data = new int[] {5, 10, 3};
+        int element = 5;
+        int result = FindLoop.indexOf(data, element);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenArrayHasNot10ThenMinus1() {
+        int[] data = new int[] {5, 0, 3, 4, -3};
+        int element = 10;
+        int result = FindLoop.indexOf(data, element);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenArrayHas10Then4() {
+        int[] data = new int[] {5, 0, 3, 4, 10, -3};
+        int element = 10;
+        int result = FindLoop.indexOf(data, element);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+}
