2be2b223f82d3ee94e237c86e92b57c4e4eea68f
Alexey Lamtyugin <leshalamtyugin@gmail.com>
2024-10-08 13:44:40 +0400

    5.6. Простые числа [#505215 #516407]

--- src/main/java/ru/job4j/loop/PrimeNumber.java
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -5,7 +5,9 @@ public class PrimeNumber {
     public static int calc(int finish) {
         int count = 0;
         for (int i = 2; i <= finish; i++) {
-            count += CheckPrimeNumber.check(i) ? 1 : 0;
+            if (CheckPrimeNumber.check(i)) {
+                ++count;
+            }
         }
         return count;
     }
