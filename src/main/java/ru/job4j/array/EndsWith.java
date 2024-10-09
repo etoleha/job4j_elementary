6edf0e48b12dfd7fd145618e688b1936f1e2c549
Alexey Lamtyugin <leshalamtyugin@gmail.com>
2024-10-09 13:45:34 +0400

    Слово заканчивается на ... [#505236 #516477]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] postfix) {
         boolean result = true;
-        for (int i = postfix.length - 1; i >= 0; --i) {
+        for (int i = 0; i < postfix.length; i++) {
             if (word[i + word.length - postfix.length] != postfix[i]) {
                 result = false;
                 break;
